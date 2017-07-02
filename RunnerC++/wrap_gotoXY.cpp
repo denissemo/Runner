c2a6c920158748a59f7a524cee3a9fc7cb92290e
denissemo <denissemo1999@gmail.com>
2017-07-02 13:24:26 +0300

    set cursor position func

--- RunnerC++/wrap_gotoXY.cpp
+++ RunnerC++/wrap_gotoXY.cpp
@@ -1,10 +1,9 @@
 #include "stdafx.h"
 #include <Windows.h>
 
-
 void gotoXY(int column, int line)
 {
-	/* #include <Windows.h>
+	/*
 	обертка позиции курсора.
 	param: column - позиция курсора в колонке
 	param: line - позиция курсора в линии
